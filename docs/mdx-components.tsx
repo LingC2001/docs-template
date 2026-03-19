9857ec7f527c3ab8adf4c4bb5c4ff8924fa53016
Ling Chen <LingC2001@gmail.com>
2026-03-20 01:54:16 +1100

    feat: add basic components

--- docs/mdx-components.tsx
+++ docs/mdx-components.tsx
@@ -1,9 +1,26 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
+import { Accordion, Accordions } from "fumadocs-ui/components/accordion";
+import { File, Files, Folder } from "fumadocs-ui/components/files";
+import { ImageZoom } from "fumadocs-ui/components/image-zoom";
+import { Step, Steps } from "fumadocs-ui/components/steps";
+import { Tab, Tabs } from "fumadocs-ui/components/tabs";
+import { TypeTable } from "fumadocs-ui/components/type-table";
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    Accordion,
+    Accordions,
+    File,
+    Files,
+    Folder,
+    ImageZoom,
+    Step,
+    Steps,
+    Tab,
+    Tabs,
+    TypeTable,
     ...components,
   };
 }
